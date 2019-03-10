b9686b893a078f50aa0f36756e191639eb71725d
xiaoqiPhoebe <48000513+xiaoqiPhoebe@users.noreply.github.com>
2019-03-10 23:52:27 +0800

    Applet项目

---
+++ HelloWorldApplet.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.applet.*;
+  public class HelloWorldApplet extends Applet{   //一个小程序
+    public void paint(Graphics g){
+        g.drawString("Hello World in Applet!",20,20);
+     }
+}
